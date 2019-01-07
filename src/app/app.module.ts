36bc9bfb1f0ee8d95edf4a53a334d3d577949661
erricoandreadev <erricoandrea.dev@gmail.com>
2019-01-07 20:21:44 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,6 +9,16 @@ import { PostComponent } from './components/post/post.component';
 import { PostChildComponent } from './components/post-child/post-child.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { UsersComponent } from './components/users/users.component';
+
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'blog', component: PostListComponent },
+  { path: 'users', component: UsersComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -17,10 +28,13 @@ import { PostListComponent } from './components/post-list/post-list.component';
     PostChildComponent,
     DashboardComponent,
     PostListComponent,
+    WelcomeComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
