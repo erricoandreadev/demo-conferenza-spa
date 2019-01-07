563e740bb465b9d0bef92f913bb6353a988ec926
erricoandreadev <erricoandrea.dev@gmail.com>
2019-01-07 18:33:54 +0100

    add service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  numPosts: number = null;
+
+  constructor(
+    private postService: PostService
+  ) { }
 
   ngOnInit() {
+    this.numPosts = this.postService.getPosts().length;
   }
 
 }
